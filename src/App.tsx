08f9a2c42fa8619d1ca14360fb59e62267dd4a60
Kyrie Nguyen <sonnhthanglong@gmail.com>
2021-01-24 15:33:57 -0500

    Re-customize loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import './App.scss';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import './App.scss';
+import MarineLoadingScreen from './components/MarineLoadingScreen';
 
-// TODO: Fix non-stop random functions
 function App() {
 
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
-        const loadingScreen = document.querySelector(".loader-container");
-        if (loadingScreen) {
-            loadingScreen.classList.add('available');
-            setTimeout(() => {loadingScreen.outerHTML = ''}, 1000);
-        }
+        setTimeout(() => setLoading(false), 3600);
     }, [])
 
-    return (
-            <div id="app">
-                <NavBar />
-                <Footer />
-            </div>
+    return (loading 
+        ? <MarineLoadingScreen />
+        : <div id="app">
+            <NavBar />
+            <Footer />
+        </div>    
     )
 }
 
